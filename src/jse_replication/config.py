0828b4e4e8f3b4a0d4c8ce311679c5d2c6c0b5e2
Vincent Selz <selz@iza.org>
2023-02-28 23:57:44 +0100

    Copy term paper to the root folder.

--- src/jse_replication/config.py
+++ src/jse_replication/config.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 
 SRC = Path(__file__).parent.resolve()
-ROOT = Path(__file__).parent.parent.resolve()
+ROOT = Path(__file__).parent.parent.parent.resolve()
 BLD = SRC.joinpath("..", "..", "bld").resolve()
