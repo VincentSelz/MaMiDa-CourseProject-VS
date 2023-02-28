59d7d1bb873cb2869ae468095edadc2226e72d43
Vincent Selz <selz@iza.org>
2023-02-28 13:24:17 +0100

    Latest updates.

--- src/jse_replication/paper/task_paper.py
+++ src/jse_replication/paper/task_paper.py
@@ -6,37 +6,26 @@ from jse_replication.config import BLD
 from jse_replication.config import ROOT
 from jse_replication.config import SRC
 
+documents = ['term_paper']
 
-documents = ["term_paper"]
-
-
-#@pytask.mark.latex(
-#    [
-#        "--pdf",
-#        "--interaction=nonstopmode",
-#        "--synctex=1",
-#        "--cd",
-#        "--quiet",
-#        "--shell-escape",
-#    ]
-#)
-#@pytask.mark.parametrize(
-#    "depends_on, produces",
-#    [
-#        (SRC / "paper" / f"{document}.tex", BLD / "paper" / f"{document}.pdf")
-#        for document in documents
-#    ],
-#)
 @pytask.mark.latex(script="term_paper.tex", document="term_paper.pdf")
-def task_compile_documents():
+def task_compile_documents( ):
     pass
 
+@pytask.mark.parametrize("depends_on, produces",
+    [
+        (SRC / "paper" / f"{document}.pdf", BLD / "paper" / f"{document}.pdf")
+        for document in documents
+    ],
+)
+def task_copy_to_bld(depends_on, produces):
+    shutil.copy(depends_on, produces)
 
-#    "depends_on, produces",
-#    [
-#        (BLD / "paper" / f"{document}.pdf", ROOT / f"{document}.pdf")
-#        for document in documents
-#    ],
-#)
-#def task_copy_to_root(depends_on, produces):
-#    shutil.copy(depends_on, produces)
+@pytask.mark.parametrize("depends_on, produces",
+    [
+        (SRC / "paper" / f"{document}.pdf", ROOT / f"VincentSelz7707241_TermPaper_MaMiDa022023.pdf")
+        for document in documents
+    ],
+)
+def task_copy_to_root(depends_on, produces):
+    shutil.copy(depends_on, produces)
